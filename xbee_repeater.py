ffebd97054049ffc444aae0b835003674d25bdf4
hmallen <allenhm@gmail.com>
2018-09-29 16:26:11 -0500

    Initial commit.

---
+++ xbee_repeater.py
@@ -0,0 +1,35 @@
+import logging
+import serial
+import time
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+serial_device = '/dev/ttyUSB0'
+serial_baudrate = 19200
+
+
+if __name__ == '__main__':
+    ser = serial.Serial(
+        port=serial_device,
+        baudrate=serial_baudrate,
+        timeout=1
+    )
+
+    while True:
+        try:
+            if ser.in_waiting > 0:
+                incoming_char = ser.read()
+                logger.debug('incoming_char: ' + str(incoming_char))
+                if incoming_char == 'H' or incoming_char == 'M':
+                    bytes_written = ser.write(incoming_char)
+                    logger.debug('bytes_written: ' + str(bytes_written))
+
+            time.sleep(0.001)
+
+        except Exception as e:
+            logger.exception(e)
+
+        except KeyboardInterrupt:
+            break
